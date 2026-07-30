f9542ccb9ccc84f394a5a599732581fbf6517754
aqin <3186538497@qq.com>
2026-07-30 19:02:47 +0800

    修改了：server业务模块-->config包-->WebMvcConfiguration配置类-->knife4j生成文档的注解内容。

--- dining-server/src/main/java/com/aqinyo/config/AliOssConfiguration.java
+++ dining-server/src/main/java/com/aqinyo/config/AliOssConfiguration.java
@@ -16,7 +16,7 @@ public class AliOssConfiguration {
     @Bean   // 设置为bean,当项目启动了,就能调用这个方法,取到方法返回出来的对象
     @ConditionalOnMissingBean   // 当没有这种bean再创建 (相当于做个判断而已)
     public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){    // 形参这里的类就是"封装好配置文件值的类",用AliOssProperties类对象get里面封装好的"aliOss的数据"出来
-        log.info("正在创建阿里云工具类对象: {}", aliOssProperties);
+        log.info("正在创建阿里云OSS工具类对象: {}", aliOssProperties);
 
         // 创建ALiOssUtil工具类对象
         return new AliOssUtil(aliOssProperties.getEndpoint(),   //TODO:使用完3个月免费额度,记得删掉阿里云的bucket
--- dining-server/src/main/java/com/aqinyo/config/WebMvcConfiguration.java
+++ dining-server/src/main/java/com/aqinyo/config/WebMvcConfiguration.java
@@ -16,6 +16,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -28,27 +29,35 @@ import java.util.List;
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Autowired
-    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;//admin的jwt令牌校验的拦截器
+    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;  //依赖注入admin端jwt令牌校验的拦截器
     @Autowired
-    private JwtTokenUserInterceptor jwtTokenUserInterceptor;//user的jwt令牌校验的拦截器
+    private JwtTokenUserInterceptor jwtTokenUserInterceptor;    //依赖注入user端jwt令牌校验的拦截器
 
-
-    /*  设置静态资源映射    (静态放行 + 下面动态拦截)  */
+    /*  设置静态资源映射	 (对于接口文档:静态放行)  (对于JWT:静态放行+ 下面动态拦截)   */
+    @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始设置静态资源映射(静态放行)...");
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        // 映射 /doc.html (Knife4j/Swagger) 的文档页面
+        registry.addResourceHandler("/doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");         // 还涉及:接口文档页面渲染所需的其他静态资源（如 css/js 等，通常还需放行 /** 到 META-INF/resources，此处仅配置了核心入口）
+
+        // 映射 /webjars下所有的前端静态资源  (如Swagger UI渲染所需的JS/CSS等)
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");         // Spring Boot 会自动将这些 jar 中的静态资源映射到 classpath:/META-INF/resources/webjars/ 目录下
     }
 
 
-    /*  注册自定义拦截器     (拦截器+注册拦截器 搭配才有用)    */      // 注册拦截器就相当于 --> 给拦截器分配任务(告诉它该干什么？)
+    /*  注册自定义JWT拦截器     (拦截器+注册拦截器 搭配才有用)    */      // 注册拦截器就相当于 --> 给拦截器分配任务(告诉它该干什么？)
     protected void addInterceptors(InterceptorRegistry registry) {
-        log.info("开始注册自定义拦截器(动态拦截)...");
+        log.info("开始注册自定义JWT拦截器(动态拦截)...");
 
-        registry.addInterceptor(jwtTokenAdminInterceptor)       // 注册 拦截器
+        // 注册 admin端JWT拦截器
+        registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")                   // 添加 以/admin开头的所有请求-->执行拦截"任务"(即如果绕过登录不获取token,则请求都为无效,然后拦截器不放行)
                 .excludePathPatterns("/admin/employee/login");  // 排除 /admin/employee/login请求的拦截"任务"
-                                                            /* 总结: 这个拦截器任务是 --> 做JWT校验拦截,没有token就不放行+报401错;只有登录这个请求是放行的,因为要放你登录拿token嘛 */
+                                                                /* 总结: 这个拦截器任务是 --> 做JWT校验拦截,没有token就不放行+报401错;只有登录这个请求是放行的,因为要放你登录拿token嘛 */
+        // 注册 user端JWT拦截器
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
@@ -57,34 +66,39 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     }
 
 
-    /*  通过 knife4j框架 自动生成接口文档 (格式可参考复用)  */
+    /*  通过 knife4j框架 自动生成 "admin端" 接口文档 (格式可参考复用)  */
     @Bean   // B端 admin的
     public Docket adminDocket() {   /*  Docket 是 springfox 的核心配置对象,代表 "一组接口文档"  */
+        log.info("开始通过knife4j框架自动生成/controller/admin的接口文档...");
 
-        ApiInfo apiInfo123 = new ApiInfoBuilder()  // ApiInfoBuilder 设置文档的 "标题、版本、描述" 等元信息
-                .title("商城外卖项目接口文档")
-                .version("2.0")
-                .description("通过knife4j框架来生成-->商城外卖项目接口文档")
+        ApiInfo apiInfo123 = new ApiInfoBuilder()  // ApiInfoBuilder设置接口文档的 "标题、作者、版本、描述" 等元信息
+                .title("餐饮服务项目接口文档")
+                .contact(new Contact("Aqinyo", "https://github.com/Aqinyo", "3186538497@qq.com"))
+                .version("1.0")
+                .description("通过 knife4j框架 来自动生成: 项目admin端的接口文档")
                 .build();
 
-        Docket docket = new Docket(DocumentationType.SWAGGER_2) //指定使用 Swagger 2 规范
-                .groupName("管理端接口") //设置 文档分组名 (可以有多个Docket，所以这里给它起个名字)
-                .apiInfo(apiInfo123)    //设置 文档元信息(即把上面 ApiInfo的对象 设置到docket中)
+        Docket docket = new Docket(DocumentationType.SWAGGER_2) //指定 使用 Swagger2 规范
+                .groupName("管理端接口")         //设置 接口文档分组名  (可以有多个Docket，所以这里给它起个名字)
+                .apiInfo(apiInfo123)           //设置 接口文档元信息  (即: 将上面 ApiInfo对象 装进docket中)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.aqinyo.controller.admin"))// 设置 只扫描这个包下的接口
-                .paths(PathSelectors.any())// 设置 该包下所有路径都纳入文档
+                .apis(RequestHandlerSelectors.basePackage("com.aqinyo.controller.admin"))   //设置 生成接口需要扫描的包
+                .paths(PathSelectors.any())    //设置 该包下所有路径都纳入接口文档
                 .build();
 
         return docket;
     }
 
+    /*  通过 knife4j框架 自动生成 "user端" 接口文档 (格式可参考复用)  */
     @Bean   // C端 user的
     public Docket userDocket() {
+        log.info("开始通过knife4j框架自动生成/controller/user的接口文档...");
 
         ApiInfo apiInfo321 = new ApiInfoBuilder()
-                .title("商城外卖项目接口文档")
-                .version("2.0")
-                .description("通过knife4j框架来生成-->商城外卖项目接口文档")
+                .title("餐饮服务项目接口文档")
+                .contact(new Contact("Aqinyo", "https://github.com/Aqinyo", "3186538497@qq.com"))
+                .version("1.0")
+                .description("通过 knife4j框架 来自动生成: 项目user端的接口文档")
                 .build();
 
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
