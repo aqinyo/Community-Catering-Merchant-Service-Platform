501d0e67b341dd3193e8c8366d1639e3708198ef
aqin <3186538497@qq.com>
2026-08-18 16:31:21 +0800

    修改了一下Readme的项目介绍内容

--- dining-common/src/main/java/com/aqinyo/utils/JwtUtil.java
+++ dining-common/src/main/java/com/aqinyo/utils/JwtUtil.java
@@ -9,16 +9,9 @@ import java.util.Date;
 import java.util.Map;
 
 public class JwtUtil {
-    /**
-     * 生成jwt
-     * 使用Hs256算法, 私匙使用固定秘钥
-     *
-     * @param secretKey jwt秘钥
-     * @param ttlMillis jwt过期时间(毫秒)
-     * @param claims    设置的信息
-     * @return
-     */
-    public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
+
+    /*   生成JWT,使用Hs256算法, 私匙使用固定秘钥   */
+    public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {  //jwt秘钥、过期时间(毫秒)、设置的信息
         // 指定签名的时候使用的签名算法，也就是header那部分
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
@@ -38,14 +31,9 @@ public class JwtUtil {
         return builder.compact();
     }
 
-    /**
-     * Token解密
-     *
-     * @param secretKey jwt秘钥 此秘钥一定要保留好在服务端, 不能暴露出去, 否则sign就可以被伪造, 如果对接多个客户端建议改造成多个
-     * @param token     加密后的token
-     * @return
-     */
-    public static Claims parseJWT(String secretKey, String token) {
+
+    /*   Token解密   */
+    public static Claims parseJWT(String secretKey, String token) {  //jwt秘钥、加密后的token
         // 得到DefaultJwtParser
         Claims claims = Jwts.parser()
                 // 设置签名的秘钥
